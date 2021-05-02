82042c0082d0ccae57932caa6e3cc203f52b445e
肖杰 <dzj201812040518@163.com>
2021-05-02 23:45:25 +0800

    日志模块列表摘要样式第一版，卡片组件插件新增自定义模板功能

--- src/main/resources/static/component/pear/modules/card.js
+++ src/main/resources/static/component/pear/modules/card.js
@@ -1,102 +1,108 @@
-layui.define(['table', 'laypage','jquery', 'element'], function(exports) {
-	"use strict";
+layui.define(['table', 'laypage', 'jquery', 'element'], function (exports) {
+    "use strict";
 
-	var MOD_NAME = 'card',
-		$ = layui.jquery,
-		element = layui.element,
-		laypage = layui.laypage;
+    var MOD_NAME = 'card',
+        $ = layui.jquery,
+        element = layui.element,
+        laypage = layui.laypage;
 
-	var pearCard = function(opt) {
-		this.option = opt;
-	};
+    var pearCard = function (opt) {
+        this.option = opt;
+    };
 
-	pearCard.prototype.render = function(opt) {
-		var option = {
-			// 构建的模型
-			elem: opt.elem,
-			// 数据 url 连接
-			url: opt.url,
-			// lineSize 每行的个数
-			lineSize: opt.lineSize ? opt.lineSize : 4,
-			// 共多少个
-			pageSize: opt.pageSize ? opt.pageSize : 12,
-			// 当前页
-			currentPage: opt.currentSize ? opt.currentSize : 0,
-			// 完 成 函 数
-			done: opt.done ? opt.done : function() {
-				alert("跳转页面");
-			}
-		}
-		// 根 据 请 求 方 式 获 取 数 据
-		if (option.url != null) {
-			// 复制数据
-			option.data = getData(option.url).data;
-		}
+    pearCard.prototype.render = function (opt) {
+        var option = {
+            // 构建的模型
+            elem: opt.elem,
+            // 数据 url 连接
+            url: opt.url,
+            // lineSize 每行的个数
+            lineSize: opt.lineSize ? opt.lineSize : 4,
+            // 共多少个 TODO 以后可能会用到
+            pageSize: opt.pageSize ? opt.pageSize : 12,
+            // 当前页
+            currentPage: opt.currentSize ? opt.currentSize : 0,
+            // 完 成 函 数
+            done: opt.done ? opt.done : function () {
+                alert("跳转页面");
+            },
+            //卡片内部模板
+            templet: opt.templet ? opt.templet : function (item) {
+                return '<img class="project-list-item-cover" src="' + item.image + '"> <div class="project-list-item-body"> <h2>' + item.title + '</h2> <div class="project-list-item-text layui-text">' + item.remark + '</div> <div class="project-list-item-desc"> <span class="time">' + item.time + '</span> <div class="ew-head-list"> <img class="ew-head-list-item" lay-tips="曲丽丽" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png"> <img class="ew-head-list-item" lay-tips="王昭君" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png"> <img class="ew-head-list-item" lay-tips="董娜娜" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png"> </div> </div> </div>'
+            }
+        }
+        // 根 据 请 求 方 式 获 取 数 据
+        if (option.url != null) {
+            // 复制数据
+            option.data = getData(option.url).data;
+        }
 
-		// 根据结果进行相应结构的创建
-		var html = createComponent(option.data);
+        // 根据结果进行相应结构的创建
+        var html = createComponent(option);
 
-		$(option.elem).html(html);
-		
-		  // 初始化分页组件
-		  laypage.render({
-		    elem: 'cardpage'
-		    ,count: 100
-		    ,layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
-		    ,jump: function(obj){
-		      console.log(obj)
-		    }
-		  });
-		  
-		return new pearCard(option);
-	}
+        $(option.elem).html(html);
 
-	function createComponent(data) {
+        // 初始化分页组件
+        laypage.render({
+            elem: 'cardpage'
+            , count: option.data.length
+            , layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
+            , jump: function (obj) {
+                console.log(obj)
+            }
+        });
+
+        return new pearCard(option);
+    }
+
+    function createComponent(options) {
         var html = "<div class='pear-card-component'>"
-        var content = createCards(data);
+        var content = createCards(options);
         var page = "<div id='cardpage'></div>"
         content = content + page;
         html += content + "</div>"
         return html;
-	}
+    }
 
 
-	/** 创建指定数量的卡片 */
-	function createCards(data) {
-		
-		var content = "<div class='layui-row layui-col-space30'>";
-		$.each(data, function(i, item) {
+    /** 创建指定数量的卡片 */
+    function createCards(options) {
 
-			content += createCard(item);
+        var content = "<div class='layui-row layui-col-space30'>";
+        $.each(options.data, function (i, item) {
 
-		})
-		content += "</div>"
-		return content;
-	}
+            content += createCard(item, options);
+
+        })
+        content += "</div>"
+        return content;
+    }
 
 
-	/** 创建一个卡片 */
-	function createCard(item) {
+    /** 创建一个卡片 */
+    function createCard(item, options) {
 
-		var card =
-			'<div class="layui-col-md3 ew-datagrid-item" data-index="0" data-number="1"> <div class="project-list-item"> <img class="project-list-item-cover" src="'+item.image+'"> <div class="project-list-item-body"> <h2>'+item.title+'</h2> <div class="project-list-item-text layui-text">'+item.remark+'</div> <div class="project-list-item-desc"> <span class="time">'+item.time+'</span> <div class="ew-head-list"> <img class="ew-head-list-item" lay-tips="曲丽丽" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png"> <img class="ew-head-list-item" lay-tips="王昭君" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png"> <img class="ew-head-list-item" lay-tips="董娜娜" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png"> </div> </div> </div> </div> </div>'
+        // var card =
+        // 	'<div class="layui-col-md3 ew-datagrid-item" data-index="0" data-number="1"> <div class="project-list-item"> <img class="project-list-item-cover" src="'+item.image+'"> <div class="project-list-item-body"> <h2>'+item.title+'</h2> <div class="project-list-item-text layui-text">'+item.remark+'</div> <div class="project-list-item-desc"> <span class="time">'+item.time+'</span> <div class="ew-head-list"> <img class="ew-head-list-item" lay-tips="曲丽丽" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png"> <img class="ew-head-list-item" lay-tips="王昭君" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png"> <img class="ew-head-list-item" lay-tips="董娜娜" lay-offset="0,-5px" src="https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png"> </div> </div> </div> </div> </div>'
+        var card = '<div class="layui-col-md' + Math.floor(12 / options.lineSize) + ' ew-datagrid-item" data-index="0" data-number="1"> <div class="project-list-item">';
+        card += options.templet(item);
+        card += '</div> </div>';
+        return card;
+    }
 
-		return card;
-	}
+    /** 同 步 请 求 获 取 数 据 */
+    function getData(url) {
 
-	/** 同 步 请 求 获 取 数 据 */
-	function getData(url) {
+        $.ajaxSettings.async = false;
+        var data = null;
 
-		$.ajaxSettings.async = false;
-		var data = null;
+        $.get(url, function (result) {
+            data = result;
+        });
 
-		$.get(url, function(result) {
-			data = result;
-		});
+        $.ajaxSettings.async = true;
+        return data;
+    }
 
-		$.ajaxSettings.async = true;
-		return data;
-	}
-
-	exports(MOD_NAME, new pearCard());
+    exports(MOD_NAME, new pearCard());
 })
