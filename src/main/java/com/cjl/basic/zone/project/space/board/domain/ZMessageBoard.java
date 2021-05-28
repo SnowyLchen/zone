c824536a2ebd9838c9811952365c5f76f1faa096
snowylchen <15293616670@163.com>
2021-05-28 20:41:12 +0800

    留言

--- src/main/java/com/cjl/basic/zone/project/space/board/domain/ZMessageBoard.java
+++ src/main/java/com/cjl/basic/zone/project/space/board/domain/ZMessageBoard.java
@@ -1,6 +1,5 @@
 package com.cjl.basic.zone.project.space.board.domain;
 
-import com.alibaba.fastjson.annotation.JSONField;
 import com.cjl.basic.zone.framework.web.domain.BaseEntity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
@@ -44,7 +43,7 @@ public class ZMessageBoard extends BaseEntity {
     /**
      * 留言时间
      */
-    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
     /**
      * 留言回复
